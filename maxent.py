84e6d2e9ee1c2c5bbb763ffe20ccef34a266160b
kinziro <kazushi@MacBook.local>
2019-10-23 19:31:31 +0900

    コメントを追記

--- maxent.py
+++ maxent.py
@@ -66,24 +66,48 @@ class MaxEntIRL():
         return features
 
     def expected_features_under_policy(self, policy, trajectories):
+        '''
+        パラメータによる報酬関数から獲得される方策による特徴ベクトルの取得
+
+        :param policy: 方策
+        :param trajectories: エキスパート軌跡  軌跡数×状態のリスト
+        :return: 各状態の発生確率 16次元のリスト
+        '''
+        # エキスパート軌跡の数, このコードではt_size=20
         t_size = len(trajectories)
         states = self.env.states
+        # 状態遷移確率 軌跡の数×状態数
         transition_probs = np.zeros((t_size, len(states)))
 
+        # 状態の発生確率(各状態の到達頻度)
         initial_state_probs = np.zeros(len(states))
+        # 各軌跡データの初期状態を取得して、数をカウント
+        # 今回の場合は初期状態は12の為、transition_probs[12]=20でそれ以外は0
         for t in trajectories:
             initial_state_probs[t[0]] += 1
+        # 回数を頻度に変換するためにt_sizeで割る
         initial_state_probs /= t_size
+        # 状態遷移確率の初期行に初期状態の配列を代入
         transition_probs[0] = initial_state_probs
 
+        # 環境の状態遷移確率にしたがって,t_size-1回状態遷移を繰り返して状態の発生確率を計算
+        # 疑問なのが、なぜ繰り返すステップ数がt_sizeで良いのか？
         for t in range(1, t_size):
+            # 1ステップ前の状態prev_sが状態0~15の場合をそれぞれ計算
+            # 1ステップ前の状態の発生確率と状態遷移確率を掛けて全て足すことで今ステップの状態発生確率を計算
+            # μ_t(s') = P(s'|s, a) * Σμ_t-1(s)
             for prev_s in states:
+                # 1ステップ前に状態prev_sにいる確率を計算
                 prev_prob = transition_probs[t - 1][prev_s]
+                # 1ステップ前の状態prev_sで行う行動を方策から決定
                 a = self.planner.act(prev_s)
+                # 状態遷移確率に従い、各状態への遷移確率を取得. probsは16次元のリスト.
                 probs = self.env.transit_func(prev_s, a)
+                # 1ステップ前の発生確率と状態遷移確率を掛ける
                 for s in probs:
                     transition_probs[t][s] += prev_prob * probs[s]
 
+        # t_sizeステップ分の発生確率を平均して、各状態の発生確率を計算
         total = np.mean(transition_probs, axis=0)
         return total
 
